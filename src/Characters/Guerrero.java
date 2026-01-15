06420f817da933c8d734a4ddcf8c7ae3c8b4be08
Jivars01 <jjivars07@gmail.com>
2026-01-15 20:51:20 +0100

    v3.1 Adaptampos el codigo a parte del ejercicio 1 de la practica y metemos la subclase guerrero

---
+++ src/Characters/Guerrero.java
@@ -0,0 +1,12 @@
+package Characters;
+
+public class Guerrero extends Personaje{
+
+
+    public Guerrero(){
+        super();
+    }
+    public Guerrero(String nombre, int pv, int atq, int arm, int nivel, int vel, int res){
+        super(nombre,pv, atq, arm, nivel,  vel, res);
+    }
+}
